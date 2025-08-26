5b6edba9133170394803d37b02e55362f24ea2c1
Michael Pearce <107821469+mmichaellll@users.noreply.github.com>
2025-08-26 01:40:04 +0000

    done thread class

--- thread.py
+++ thread.py
@@ -1,3 +1,5 @@
+from exceptions import PermissionDenied
+
 class Thread:
   def __init__(self, title, first_post):
     """
@@ -5,37 +7,46 @@ class Thread:
     The author of the first post is also the owner of the thread.
     The owner cannot change once the thread is created.
     """
-    pass
+
+    self.title = title
+    self.first_post = first_post
+    self.owner = first_post.get_author()
+    self.tags = {}
+    self.posts = [first_post]
 
   def get_owner(self):
     """
     Returns the owner of the thread.
     """
-    pass
+    return self.first_post.get_author()
   
   def get_title(self):
     """
     Returns the title of the thread.
     """
-    pass
+    return self.title
   
   def get_tags(self):
     """
-    Returns a sorted list of unique tags.
+    Returns a alphabetically sorted list of unique tags.
     """
-    pass
+    return self.tags.sorted()
   
   def get_posts(self):
     """
     Returns a list of posts in this thread, in the order they were published.
     """
-    pass
+    return self.posts
   
   def publish_post(self, post):
     """
     Adds the given post object into the list of posts.
     """
-    pass
+    try:
+      self.posts.append(post)
+      return True
+    except:
+      return False
   
   def remove_post(self, post, by_user):
     """
@@ -43,18 +54,29 @@ class Thread:
     Does nothing if the post is not in this thread.
     * raises PermissionDenied if the given user is not the author of the post.
     """
-    pass
-  
+    if post in self.posts and post.get_author() == by_user:
+      self.posts.remove(post)
+      return True
+    else:
+      raise PermissionDenied()        
+
   def set_title(self, title, by_user):
     """
     Allows the given user to edit the thread title.
     * raises PermissionDenied if the given user is not the owner of the thread.
     """
-    pass
+  
+    if self.get_owner() == by_user:
+      self.title = title
+    else:
+      raise PermissionDenied()
   
   def set_tags(self, tag_list, by_user):
     """
     Allows the given user to replace the thread tags (list of strings).
     * raises PermissionDenied if the given user is not the owner of the thread.
     """
-    pass
+    if self.get_owner() == by_user:
+      self.tags = tag_list
+    else:
+      raise PermissionDenied()
