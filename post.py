58e985023dd2df9af50572e142aafc7f16518987
Arabella Simpson <78235705+boatartist@users.noreply.github.com>
2025-08-26 01:10:53 +0000

    implemented basic methods for Post class

--- post.py
+++ post.py
@@ -5,36 +5,41 @@ class Post:
     The author of the first post at the time of thread creation is the owner of the thread.
     The owner cannot change once the thread is created.
     """
-    pass
+    self.content = content
+    self.author = author
+    self.upvotes = set()
   
   def get_author(self):
     """
     Returns the author of the post.
     """
-    pass
+    return self.author
   
   def get_content(self):
     """
     Returns the content of the post.
     """
-    pass
+    return self.content
   
   def get_upvotes(self):
     """
     Returns a single integer representing the total number of upvotes.
     """
-    pass
+    return len(self.upvotes())
   
   def set_content(self, content, by_user):
     """
     Called when the given user wants to update the content.
     * raises PermissionDenied if the given user is not the author.
     """
-    pass
+    if by_user == self.author:
+      self.content = content
+    else:
+      raise(PermissionDenied)
   
   def upvote(self, by_user):
     """
     Called when the given user wants to upvote this post.
     A user can only perform an up vote *once*.
     """
-    pass
+    self.upvotes.add(by_user)
